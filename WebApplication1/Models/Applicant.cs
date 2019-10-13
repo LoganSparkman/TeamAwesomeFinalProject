e34e9e5feae1f8abcc7f92f115867417a45203d0
michaelneill1 <m.neill19@gmail.com>
2019-10-13 09:58:10 -0600

    Added remaining models, just missing login

---
+++ WebApplication1/Models/Applicant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Applicant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string Fname { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lname { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "Public School Level")]
+        public int SchoolLevel { get; set; }
+
+        [Display(Name = "Guardian Name")]
+        public string GuardianName { get; set; }
+
+        [Display(Name = "Guardian Type")]
+        public string GuardianType { get; set; }
+
+        public string Address { get; set; }
+        public string Village { get; set; }
+        public string GPS { get; set; }
+        public string Phone { get; set; }
+        public string Photo { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Term.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Term
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime TermStart  { get; set; }
+
+        [Required]
+        public DateTime TermEnd { get; set; }
+
+        [Display(Name = "Term")]
+        public string TermName { get; set; }
+    }
+}
