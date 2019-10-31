0905c8316598d8dd475e42715fbdfda1f7b6f6ca
michaelneill1 <m.neill19@gmail.com>
2019-10-31 11:13:29 -0600

    Added paging and searching to student page, working on being able to add classes to students schedule

---
+++ WebApplication1/Pages/Students/ChangeSchedule.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Students
+{
+    public class ChangeScheduleModel : PageModel
+    {
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+
+        public ChangeScheduleModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student
+                .Include(s => s.StudentStatus).FirstOrDefaultAsync(m => m.StudentID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+           ViewData["StudentStatusID"] = new SelectList(_context.StudentStatus, "StudentStatusID", "Name");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(Student.StudentID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Any(e => e.StudentID == id);
+        }
+    }
+}
--- WebApplication1/Pages/Students/Index.cshtml.cs
+++ WebApplication1/Pages/Students/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -19,12 +20,50 @@ namespace WebApplication1.Pages.Students
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Student> Student { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+        string currentFilter, string searchString, int? pageIndex)
         {
-            Student = await _context.Student
-                .Include(s => s.StudentStatus).ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentIQ = from s in _context.Student
+                                            select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentIQ = studentIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentIQ = studentIQ.OrderByDescending(s => s.LastName);
+                    break;
+                default:
+                    studentIQ = studentIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 5;
+            Student = await PaginatedList<Student>.CreateAsync(
+                studentIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
