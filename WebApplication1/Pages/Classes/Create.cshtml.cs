cf9aa693ce7ceaac8d7bc1c76d53ef9ba10d6341
michaelneill1 <m.neill19@gmail.com>
2019-11-03 13:50:05 -0700

    Added functionality that allows class creation to also select the class time, then on the post create it creates the class and also populates the class schedule table, whcih it wasn't doing before

--- WebApplication1/Pages/Classes/Create.cshtml.cs
+++ WebApplication1/Pages/Classes/Create.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Models;
 
@@ -14,6 +15,19 @@ namespace WebApplication1.Pages.Classes
     {
         private readonly WebApplication1.Data.ApplicationDbContext _context;
 
+        [BindProperty]
+        public IEnumerable<Term> Terms { get; set; }
+        [BindProperty]
+        public IEnumerable<Course> Courses { get; set; }
+        [BindProperty]
+        public IList<Schedule> Schedules { get; set; }
+        [BindProperty]
+        public IList<ScheduleHelper> Schedules2 { get; set; }
+
+        public string FirstDay = "First Day";
+
+        public string SecondDay = "Second Day";
+
         public CreateModel(WebApplication1.Data.ApplicationDbContext context)
         {
             _context = context;
@@ -21,25 +35,57 @@ namespace WebApplication1.Pages.Classes
 
         public IActionResult OnGet()
         {
-        ViewData["CourseID"] = new SelectList(_context.Course, "CourseID", "Name");
-        ViewData["TermID"] = new SelectList(_context.Term, "TermID", "Description");
+            Schedules = _context.Schedule.ToList();
+            Schedules2 = new List<ScheduleHelper>();
+
+            foreach (Schedule Schedule in Schedules)
+            {
+                ScheduleHelper ScheduleHelp = new ScheduleHelper();
+                ScheduleHelp.ScheduleID = Schedule.ScheduleID;
+                ScheduleHelp.ListHelper = Schedule.DayOfWeek.ToString() + " " + Schedule.StartTime.ToString("hh:mm tt");
+                Schedules2.Add(ScheduleHelp);
+            }
+
+            ViewData["CourseID"] = new SelectList(_context.Course, "CourseID", "Name");
+            ViewData["TermID"] = new SelectList(_context.Term, "TermID", "TermID");
+            ViewData["ScheduleID"] = new SelectList(Schedules2, "ScheduleID", "ListHelper");
             return Page();
         }
 
         [BindProperty]
         public Class Class { get; set; }
+        [BindProperty]
+        public ClassSchedule ScheduleFirstDay{ get; set; }
+        [BindProperty]
+        public ClassSchedule ScheduleSecondDay { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
 
             _context.Class.Add(Class);
+           
+            await _context.SaveChangesAsync();
+
+            //goes in and finds the last record in the table so we can use the id to create our two class schedule records
+            int classID = _context.Class
+                    .OrderByDescending(c => c.ClassID)
+                    .Select(r => r.ClassID)
+                    .First();
+
+            ScheduleFirstDay.ClassID = classID;
+            ScheduleSecondDay.ClassID = classID;
+            _context.ClassSchedule.Add(ScheduleFirstDay);
+            _context.ClassSchedule.Add(ScheduleSecondDay);
+
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }
+    }
+
+    public class ScheduleHelper
+    {
+        public int ScheduleID { get; set; }
+        public string ListHelper { get; set; }
     }
 }
