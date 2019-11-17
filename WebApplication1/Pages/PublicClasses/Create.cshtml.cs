1e6ebfc666eff24c36c2445454ef4faef31684ed
michaelneill1 <m.neill19@gmail.com>
2019-11-17 11:09:11 -0700

    Updated the student schedule page to filter classes based on there level in english and it, only displaying the classes they need

---
+++ WebApplication1/Pages/PublicClasses/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.PublicClasses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+
+        public CreateModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["StudentID"] = new SelectList(_context.Student, "StudentID", "FirstName");
+            return Page();
+        }
+
+        [BindProperty]
+        public StudentPublicSchoolClass StudentPublicSchoolClass { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.StudentPublicSchoolClass.Add(StudentPublicSchoolClass);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ WebApplication1/Pages/PublicClasses/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.PublicClasses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+
+        public DetailsModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public StudentPublicSchoolClass StudentPublicSchoolClass { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            StudentPublicSchoolClass = await _context.StudentPublicSchoolClass
+                .Include(s => s.Student).FirstOrDefaultAsync(m => m.StudentPublicSchoolClassID == id);
+
+            if (StudentPublicSchoolClass == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
