ab0e6828ab78d7c46b9c46cb04f79afaca654b35
michaelneill1 <m.neill19@gmail.com>
2019-11-17 19:32:05 -0700

    Attendance can now read from csv file

--- WebApplication1/Pages/Attendance/EnterAttendance.cshtml.cs
+++ WebApplication1/Pages/Attendance/EnterAttendance.cshtml.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApplication1.Utility;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
 
 namespace WebApplication1.Pages.Attendance
 {
@@ -24,6 +27,26 @@ namespace WebApplication1.Pages.Attendance
         public void OnGet(int classId)
         {
             this.classId = classId;
+        }
+
+        public IActionResult OnPostImport(IFormFile postedFile)
+        {
+            using (ExcelPackage package = new ExcelPackage(postedFile.OpenReadStream()))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = worksheet.Dimension.End.Row;     //get row count
+
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    for (int col = 1; col <= colCount; col++)
+                    {
+                        string currentThing = " Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value?.ToString().Trim();
+                    }
+                }
+            }
+
+            return RedirectToPage("./ClassList");
         }
     }
 }
