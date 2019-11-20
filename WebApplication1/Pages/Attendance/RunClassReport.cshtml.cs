05dd371d646cefb70f1b510c84c6f29cbd656ba4
michaelneill1 <m.neill19@gmail.com>
2019-11-20 16:26:39 -0700

    Fixed the buttons on the attendance page, also tweaked some things on the attendance chart

--- WebApplication1/Pages/Attendance/RunClassReport.cshtml.cs
+++ WebApplication1/Pages/Attendance/RunClassReport.cshtml.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1.Pages.Attendance
 {
     public class RunClassReportModel : PageModel
     {
+        public Course Course { get; set; }
+
+        public IList<Class> Class { get; set; }
+
         private readonly WebApplication1.Data.ApplicationDbContext _context;
         public RunClassReportModel(WebApplication1.Data.ApplicationDbContext context)
         {
@@ -17,9 +23,21 @@ namespace WebApplication1.Pages.Attendance
 
         public int classId { get; set; }
 
-        public void OnGet(int classId)
+        public async Task OnGetAsync(int classId)
         {
             this.classId = classId;
+
+            Class =  await _context.Class
+                    .Where(c => c.ClassID == classId)
+                    .ToListAsync();
+
+            for(int i = 0; i < Class.Count; i++)
+            {
+                Course = await _context.Course
+                        .Where(c => c.CourseID == Class[i].CourseID)
+                        .FirstOrDefaultAsync();
+            }
+
         }
     }
 }
