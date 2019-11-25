026baa1e32bb646c1bcdde9999dc8bae9e780347
michaelneill1 <m.neill19@gmail.com>
2019-11-24 19:56:29 -0700

    Added a datepicker to the manual attendance entry

---
+++ WebApplication1/Pages/Attendance/ManualAttendanceEdit.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Attendance
+{
+    public class ManualAttendanceEditModel : PageModel
+    {
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+        public ManualAttendanceEditModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public int classId { get; set; }
+
+        public void OnGet(int classId)
+        {
+            this.classId = classId;
+        }
+
+        public async Task<IActionResult> OnPostGetAttendance(string date)
+        {
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./ManualAttendanceEdit");
+        }
+    }
+}
