823dad6ac25305138a09097bce88dc5dc8d78f6d
logansparkman <logansparkman@weber.edu>
2019-11-25 03:59:02 -0700

    Grade Revamp Part 2 - Can now edit grades by assessment.

--- WebApplication1/Pages/Grades/GradesByAssessment.cshtml.cs
+++ WebApplication1/Pages/Grades/GradesByAssessment.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Pages.Grades
@@ -17,16 +18,69 @@ namespace WebApplication1.Pages.Grades
         {
             _context = context;
         }
-        public IList<StudentAssessment> StudentAssessment { get; set; }
-        public async Task<IActionResult> OnGet(int AssessmentId)
+
+        [BindProperty]
+        public List<StudentAssessment> StudentAssessment { get;set; }
+
+        public async Task<IActionResult> OnGetAsync(int? AssessmentId)
         {
+            if (AssessmentId == null)
+            {
+                return NotFound();
+            }
             Assessment thisAssessment = await _context.Assessment
-                .Include(c => c.Class)
                 .Where(a => a.AssessmentId == AssessmentId)
                 .FirstOrDefaultAsync();
-
-
+            StudentAssessment = await _context.StudentAssessment
+                .Include(s => s.Assessment)
+                .Include(s => s.Student)
+                .Where(a => a.AssessmentID == AssessmentId).ToListAsync();
+            int i = 0;
+            //Add grades into database if not already there
+            List<StudentClass> Students = await _context.StudentClass
+                .Where(c => c.ClassID == thisAssessment.ClassID)
+                .ToListAsync();
+            /*while (StudentAssessment.Count != assessments.Count)
+            {
+                StudentAssessment sa = await _context.StudentAssessment
+                    .Where(s => s.StudentID == studentId)
+                    .Where(a => a.AssessmentID == assessments[i].AssessmentId)
+                    .FirstOrDefaultAsync();
+                if (sa == null)
+                {
+                    sa = new StudentAssessment
+                    {
+                        AssessmentID = assessments[i].AssessmentId,
+                        StudentID = studentId,
+                        Comment = ""
+                    };
+                    _context.StudentAssessment.Add(sa);
+                    await _context.SaveChangesAsync();
+                }
+                i++;
+                StudentAssessment = await _context.StudentAssessment
+                    .Include(s => s.Student)
+                    .Where(s => s.StudentID == studentId)
+                    .Where(s => s.Assessment.ClassID == classId)
+                    .ToListAsync();
+            }*/
             return Page();
+        }
+        public async Task OnPostAsync()
+        {
+            List<StudentAssessment> FromDB = await _context.StudentAssessment
+                .Include(s => s.Assessment)
+                .Include(s => s.Student)
+                .Where(a => a.AssessmentID == StudentAssessment[0].AssessmentID).ToListAsync();
+            for (int i = 0; i < FromDB.Count; i++)
+            {
+                FromDB[i].StudentID = StudentAssessment[i].StudentID;
+                FromDB[i].PointsAwarded = StudentAssessment[i].PointsAwarded;
+                FromDB[i].AssessmentID = StudentAssessment[i].AssessmentID;
+                FromDB[i].Comment = StudentAssessment[i].Comment;
+            }
+            await _context.SaveChangesAsync();
+            await OnGetAsync(StudentAssessment[0].AssessmentID);
         }
     }
 }
