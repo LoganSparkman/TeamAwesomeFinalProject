73c4c0bf29433efdcd24188b72fca8c8e6b8d975
michaelneill1 <m.neill19@gmail.com>
2019-11-24 18:20:44 -0700

    Added an individual attendance analysis that can be accessed from the student details page

---
+++ WebApplication1/Pages/Students/AttendanceAnalysis.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Students
+{
+    public class AttendanceAnalysisModel : PageModel
+    {
+        public int attended;
+        public int late;
+        public int absent;
+
+        public Student Student { get; set; }
+
+        public IList<Models.Attendance> Attendance { get; set; }
+
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+        public AttendanceAnalysisModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public int studentid { get; set; }
+
+        public async Task OnGetAsync(int studentid)
+        {
+            this.studentid = studentid;
+
+            Attendance = await _context.Attendance
+                         .Where(s => s.StudentID == studentid)
+                         .ToListAsync();
+
+            Student = await _context.Student
+                        .Where(s => s.StudentID == studentid)
+                        .FirstOrDefaultAsync();
+
+            foreach (var attendance in Attendance)
+            {
+                if (attendance.AttendanceStatusID == 1)
+                {
+                    attended++;
+                }
+                else if (attendance.AttendanceStatusID == 2)
+                {
+                    late++;
+                }
+                else
+                {
+                    absent++;
+                }
+            }
+        }
+    }
+}
