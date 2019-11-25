79adfb0e35c8f98eb7ea2e5a46191ff70237b559
logansparkman <logansparkman@weber.edu>
2019-11-25 01:33:57 -0700

    Grade Reset Part 1

---
+++ WebApplication1/Pages/Grades/AssessmentList.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Grades
+{
+    public class AssessmentListModel : PageModel
+    {
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+
+        public AssessmentListModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Assessment> Assessment { get;set; }
+
+        public async Task<IActionResult> OnGetAsync(int? ClassId)
+        {
+            if (ClassId == null)
+            {
+                return NotFound();
+            }
+            Assessment = await _context.Assessment
+                .Include(a => a.Class)
+                .Where(c => c.ClassID == ClassId).ToListAsync();
+            return Page();
+        }
+    }
+}
---
+++ WebApplication1/Pages/Grades/GradesByAssessment.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Grades
+{
+    public class GradesByAssessmentModel : PageModel
+    {
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+
+        public GradesByAssessmentModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IList<StudentAssessment> StudentAssessment { get; set; }
+        public async Task<IActionResult> OnGet(int AssessmentId)
+        {
+            Assessment thisAssessment = await _context.Assessment
+                .Include(c => c.Class)
+                .Where(a => a.AssessmentId == AssessmentId)
+                .FirstOrDefaultAsync();
+
+
+            return Page();
+        }
+    }
+}
